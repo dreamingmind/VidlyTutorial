4487912037cc875a80c3176d97f66433683ea382
dreamingmind <ddrake@dreamingmind.com>
2023-03-02 10:53:50 -0800

    begin sketching in api edit (patch :controller/:id)

--- controllers/GenresController.js
+++ controllers/GenresController.js
@@ -10,7 +10,6 @@ class GenresController
     }
 
     /**
-     *
      * @param id
      * @returns {{name: string, id: number}|{name: string, id: number}} | undefined
      */
@@ -18,11 +17,29 @@ class GenresController
         return this.findById(id);
     }
 
+    edit = function(id) {
+        let index = this.findIndexOf(id);
+        if (!index) return false;
+        /**
+         * We need the request in here to do the edit.
+         * The best approach is probably to send it as a constructor arg.
+         * We could also send the response object for completeness.
+         */
+    }
+
     findById(id) {
         return GenresController.data.find(
-            (element, index, collection) => {
-                if (element.id === +id) return element;
+            (value, index, collection) => {
+                if (value.id === +id) return value;
             })
+    }
+    
+    findIndexOf(id) {
+        return GenresController.data.find(
+            (value,index,obj) => {
+                if (value.id === +id) return index;
+            }
+        )
     }
 }
 
