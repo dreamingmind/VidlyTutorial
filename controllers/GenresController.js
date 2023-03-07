3d91ca5fd0304c5fecd1d69063d19a650c38c8c9
dreamingmind <ddrake@dreamingmind.com>
2023-03-07 12:01:08 -0800

    remove notes, now resolved

--- controllers/GenresController.js
+++ controllers/GenresController.js
@@ -30,8 +30,6 @@ class GenresController
     }
 
     /**
-     * @todo This should validate 'data' in a 'Table'?
-     *
      * @param {{name}} data
      * @returns {{name, id: *}}
      */
@@ -49,20 +47,6 @@ class GenresController
 
         this.data.push(genre);
         return genre;
-        // let index = this.findIndexOf(id);
-        // if (!index) return false;
-        /**
-         * We need the request in here to do the edit.
-         * The best approach is probably to send it as a constructor arg.
-         * We could also send the response object for completeness.
-         *
-         * I've updated the constructor, but it does nothing yet
-         * because the 'new GenresController' is in our export
-         * and so, the values never get called.
-         *
-         * The calling/requiring code knows the req/res, so
-         * we'll have to move the construction/new call there.
-         */
     }
 
     findById(id) {
